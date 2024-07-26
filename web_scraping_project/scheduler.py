ca4d957c10b3720bfa2f01b1cd8436287ed2e983
SergioSierraMelero <sergiosm365@gmail.com>
2024-07-26 14:00:23 +0200

    logs en schedule

--- web_scraping_project/scheduler.py
+++ web_scraping_project/scheduler.py
@@ -1,13 +1,37 @@
 import schedule
 import time
-from web_scraping_project.scraper import QuoteScraper
+import logging
+import logging.config
+from web_scraping_project.scraper import QuoteScraper  # Asegúrate de que este sea el import correcto
+
+# Cargar la configuración de logging desde logging.conf
+try:
+    logging.config.fileConfig('web_scraping_project/logging.conf', disable_existing_loggers=False)
+except FileNotFoundError:
+    print("El archivo de configuración de logging 'logging.conf' no se encuentra.")
+except Exception as e:
+    print(f"Error al configurar el logging: {e}")
+
+# Obtener el logger específico
+logger = logging.getLogger('scheduler')
 
 def job():
-    scraper = QuoteScraper()
-    scraper.scrape_quotes()
+    logger.info("Inicio del trabajo de scraping.")
+    try:
+        scraper = QuoteScraper()
+        scraper.scrape_quotes()
+        logger.info("Trabajo de scraping completado exitosamente.")
+    except Exception as e:
+        logger.error(f"Error durante el trabajo de scraping: {e}")
 
+# Programar el trabajo para que se ejecute todos los días a medianoche
 schedule.every().day.at("00:00").do(job)
+logger.info("Trabajo de scraping programado para ejecutarse todos los días a medianoche.")
 
+logger.info("Scheduler iniciado, esperando para ejecutar trabajos.")
+
+# Bucle infinito para mantener el script en ejecución y verificar trabajos pendientes
 while True:
     schedule.run_pending()
+    logger.debug("Ejecutando trabajos programados si los hay.")
     time.sleep(60)  # Espera un minuto antes de verificar nuevamente
